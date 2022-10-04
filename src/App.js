3bb05ab17217245240bd953f96eaa29b945fe63c
Yike Gao <yikegao8@gmail.com>
2022-10-04 23:07:05 +0100

    10.4

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import Discover from './pages/Discover';
 function App() {
   return (
   
-   <div className="App">
-      Hello 
-     <Routes>
-      <Route path='/' element={<Discover/>}>
+   <div className="relative flex">
+    <div className='flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]"'>
+    <div className='px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse'>
+        <div className='flex-1 h-fit pb-40'>
+        <Routes>
+      <Route path='/' element={<Discover/>} />
 
-      </Route>
      </Routes>
+        </div>
+    
+      </div>
+    </div>
+  
+    
     </div>
 
    
